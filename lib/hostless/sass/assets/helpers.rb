2e2881648347b28332dce3bc2e9c6ef3a416203d
Jeff Dean <jeff@zilkey.com>
2013-09-19 13:08:18 -0600

    upgrade to support sass-rails 4+

--- lib/hostless/sass/assets/helpers.rb
+++ lib/hostless/sass/assets/helpers.rb
@@ -1,26 +1,25 @@
-module Hostless::Sass::Assets::Helpers
-  include Sass::Rails::Helpers
+module Sprockets
+  module SassFunctions
 
-  def hostless_asset_path(asset, kind, append = Sass::Script::String.new(""))
-    append = append.to_s.gsub("\"","")
-    path = public_path(asset.value, kind.value)
-    if append.include?("?") && path.include?("?")
-      append.gsub!("?", "")
+    def hostless_asset_path(asset, kind, append = Sass::Script::String.new(""))
+      append = append.to_s.gsub("\"", "")
+      path = public_path(asset.value, kind.value)
+      if append.include?("?") && path.include?("?")
+        append.gsub!("?", "")
+      end
+      path << append
+
+      if ::Rails.application.config.action_controller.asset_host.present?
+        path.sub!(::Rails.application.config.action_controller.asset_host, "")
+      end
+
+      Sass::Script::String.new(path)
     end
-    path << append
 
-    if ::Rails.application.config.action_controller.asset_host.present?
-      path.sub!(::Rails.application.config.action_controller.asset_host, "")
+    def hostless_asset_url(asset, kind, append = Sass::Script::String.new(""))
+      Sass::Script::String.new(%Q{url(#{hostless_asset_path(asset, kind, append).value})})
     end
 
-    Sass::Script::String.new(path)
   end
 
-  def hostless_asset_url(asset, kind, append = Sass::Script::String.new(""))
-    Sass::Script::String.new(%Q{url(#{hostless_asset_path(asset, kind, append).value})})
-  end
-end
-
-module Sass::Script::Functions
-  include Hostless::Sass::Assets::Helpers
 end
--- lib/hostless/sass/assets/version.rb
+++ lib/hostless/sass/assets/version.rb
@@ -1,7 +1,7 @@
 module Hostless
   module Sass
     module Assets
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
     end
   end
 end
